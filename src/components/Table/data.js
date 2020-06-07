f1c77fd7a3bd4de9eff41f6ec1856f02fa1f6b90
duong bao tran <bao.tran@m1studio.co>
2020-06-07 16:38:52 +0700

    up data mockup

---
+++ src/components/Table/data.js
@@ -0,0 +1,90 @@
+import { v4 as uuid } from 'uuid';
+
+export const mockup = {
+  "stats": [
+    {
+      "id": uuid(),
+      "name": "Mike",
+      "point": 1230,
+      "level": 80,
+      "star": 3
+    },
+    {
+      "id": uuid(),
+      "name": "Alex",
+      "point": 24,
+      "level": 50,
+      "star": 2
+    },
+    {
+      "id": uuid(),
+      "name": "Christopher",
+      "point": 0,
+      "level": 20,
+      "star": 1
+    },
+    {
+      "id": uuid(),
+      "name": "John",
+      "point": -60,
+      "level": 0,
+      "star": 0
+    },
+    {
+      "id": uuid(),
+      "name": "Mike",
+      "point": 1230,
+      "level": 80,
+      "star": 3
+    },
+    {
+      "id": uuid(),
+      "name": "Alex",
+      "point": 24,
+      "level": 50,
+      "star": 2
+    },
+    {
+      "id": uuid(),
+      "name": "Christopher",
+      "point": 0,
+      "level": 20,
+      "star": 1
+    },
+    {
+      "id": uuid(),
+      "name": "John",
+      "point": -60,
+      "level": 0,
+      "star": 0
+    },
+    {
+      "id": uuid(),
+      "name": "Mike",
+      "point": 1230,
+      "level": 80,
+      "star": 3
+    },
+    {
+      "id": uuid(),
+      "name": "Alex",
+      "point": 24,
+      "level": 50,
+      "star": 2
+    },
+    {
+      "id": uuid(),
+      "name": "Christopher",
+      "point": 0,
+      "level": 20,
+      "star": 1
+    },
+    {
+      "id": uuid(),
+      "name": "John",
+      "point": -60,
+      "level": 0,
+      "star": 0
+    }
+  ]
+}
