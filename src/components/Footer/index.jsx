101c9b532d48715f7daae6dd9318b8ae3282737a
duong bao tran <bao.tran@m1studio.co>
2020-06-07 10:48:10 +0700

    add footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,6 +5,8 @@ import * as S from './styled';
 const Footer = () => {
   return (
     <S.Footer>
+      <S.Title>Extend Human Potential</S.Title>
+      <S.Desc>At Cinnamon we are working to make a world where human creativity can flourish by using our AI technology to replace all the repetitive, mind-numbing tasks that take place today.</S.Desc>
     </S.Footer>
   )
 }
--- src/components/Footer/styled.jsx
+++ src/components/Footer/styled.jsx
@@ -3,7 +3,18 @@ import styled, { css } from 'styled-components'
 export const Footer = styled.div`
   position: fixed;
   bottom: 0;
-  height: 50px;
-  background: black;
   width: 100%;
+  padding: 10rem;
+  background: #fff;
+  border-top: 1px solid #eee;
+  text-align: center;
 `;
+export const Title = styled.p`
+  color: #bdbdbd;
+  font-size: 12rem;
+  margin-bottom: 0.5em;
+`
+export const Desc = styled.p`
+  color: #bdbdbd;
+  font-size: 12rem;
+`
